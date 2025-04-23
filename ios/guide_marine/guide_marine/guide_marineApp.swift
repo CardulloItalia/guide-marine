64dd16316c8e00cba2ef2e9d9d2268da00bded7a
Italia <cardullo.lia@gmail.com>
2025-04-23 12:13:16 +0200

    Start: Initial Project

---
+++ ios/guide_marine/guide_marine/guide_marineApp.swift
@@ -0,0 +1,17 @@
+//
+//  guide_marineApp.swift
+//  guide_marine
+//
+//  Created by Italia Maria Desiré Cardullo on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct guide_marineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
